e6488c6d34d4d8448fbc5a8889cdf8d394732da8
James Narayanan <jamesnarayanan@gmail.com>
2018-08-05 12:06:48 -0400

    Formatted article pages
    Used both css and js

---
+++ articleScript.js
@@ -0,0 +1,21 @@
+function init() {
+	onResize();
+}
+function onResize() {
+	var split = document.getElementsByClassName('split');
+	var header = document.getElementById('header');
+	var headHeight = parseInt(getStyle(header, 'height'), 10);
+	var newHeight = headHeight + 55 + "px"; //+40 makes it go directly under the header
+	split[0].style.top = newHeight;
+	split[1].style.top = newHeight;
+
+
+	split[0].style.height = window.innerHeight - headHeight - 88 + "px"; //Minus 75 makes it fill to the bottom with a +40 height
+	split[1].style.height = window.innerHeight - headHeight - 88 + "px";
+}
+function getStyle(el,styleProp) { //https://stackoverflow.com/questions/10556185/style-backgroundcolor-is-an-empty-string-in-javascript
+    if (el.currentStyle)
+        return el.currentStyle[styleProp];
+
+    return document.defaultView.getComputedStyle(el,null)[styleProp];
+}
